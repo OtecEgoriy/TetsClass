7d1a02970ae8089cddc70e06285426d190113b92
OtecEgoriy <EgorA99@yandex.ru>
2020-01-03 09:38:29 +0000

    Step_1

---
+++ main.py
@@ -0,0 +1,14 @@
+class Car():
+  def __init__(self, name, color, year):
+    self.name = name
+    self.color = color
+    self.year = year
+
+  def drive (self):
+    print('Car driving')
+
+opel_car = ('Opel','Blue','1993')
+opel_car.drive()
+print(opel_car.name)
+print(opel_car.color)
+print(opel_car.year)
